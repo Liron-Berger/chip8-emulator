b056905184164862f47856c1ddd82933fd6ff3d0
Liron-Berger <lironberger13@gmail.com>
2020-06-27 17:05:15 +0300

    Add instructions until 0x8

--- src/instructions.rs
+++ src/instructions.rs
@@ -2,14 +2,58 @@ extern crate gl;
 use crate::cpu::Cpu;
 
 pub fn get_opcode_func(opcode: u16) -> fn(&mut Cpu, u16) {
-    match opcode {
-        0x00E0 => op_00e0,
-        0x00EE => op_00ee,
-        0x1000..=0x1fff => op_1nnn,
-        0x2000..=0x2fff => op_2nnn,
+    match (opcode & 0xf000) >> 12 {
+        0x0 => op_0(opcode),
+        0x1 => default,
+        0x2 => op_2nnn,
+        0x3 => op_3xnn,
+        0x4 => op_4xnn,
+        0x5 => op_5xy0,
+        0x6 => op_6xnn,
+        0x7 => op_7xnn,
+        0x8 => op_8(opcode),
         _ => default,
     }
 }
+
+fn get_opcode_parts(opcode: u16) -> (u16, u8, u8, u8, u8) {
+    let nnn = opcode & 0x0fff;
+    let kk = (opcode & 0xff) as u8;
+    let n = (opcode & 0xf) as u8;
+    let x = ((opcode & 0xf00) >> 8) as u8;
+    let y = ((opcode & 0xf0) >> 4) as u8;
+    (nnn, kk, n, x, y)
+}
+
+fn op_0(opcode: u16) -> fn(&mut Cpu, u16) {
+    let (_, kk, _, _, _) = get_opcode_parts(opcode);
+    match kk {
+        0xe0 => op_00e0,
+        0xee => op_00ee,
+        _ => default
+    }
+}
+
+fn op_8(opcode: u16) -> fn(&mut Cpu, u16) {
+    let (_, _, n, _, _) = get_opcode_parts(opcode);
+    match n {
+        0x0 => op_8xy0,
+        0x1 => op_8xy1,
+        0x2 => op_8xy2,
+        0x3 => op_8xy3,
+        0x4 => op_8xy4,
+        0x5 => op_8xy5,
+        0x6 => op_8xy6,
+        0x7 => op_8xy7,
+        0xe => op_8xye,
+        _ => default
+    }
+}
+
+fn op_9(opcode: u16) -> fn(&mut Cpu, u16) {
+    op_3xnn
+}
+
 
 #[allow(unused_variables)]
 #[allow(dead_code)]
@@ -40,33 +84,52 @@ fn op_2nnn(cpu: &mut Cpu, opcode: u16) {
     println!("sp in now {}", cpu.sp);
 }
 
-#[allow(unused_variables)]
-#[allow(dead_code)]
-fn op_3xnn(cpu: &mut Cpu, opcode: u16) {}
+fn op_3xnn(cpu: &mut Cpu, opcode: u16) {
+    let (_, kk, _, x, _) = get_opcode_parts(opcode);
+    println!("op_3xnn {} {}", x, kk);
+    if cpu.registers[x as usize] == kk {
+        cpu.pc += 2;
+    }
+}
 
-#[allow(unused_variables)]
-#[allow(dead_code)]
-fn op_4xnn(cpu: &mut Cpu, opcode: u16) {}
+fn op_4xnn(cpu: &mut Cpu, opcode: u16) {
+    let (_, kk, _, x, _) = get_opcode_parts(opcode);
+    println!("op_4xnn {} {}", x, kk);
+    if cpu.registers[x as usize] != kk {
+        cpu.pc += 2;
+    }
+}
 
-#[allow(unused_variables)]
-#[allow(dead_code)]
-fn op_5xy0(cpu: &mut Cpu, opcode: u16) {}
+fn op_5xy0(cpu: &mut Cpu, opcode: u16) {
+    let (_, _, _, x, y) = get_opcode_parts(opcode);
+    println!("5xy0 {} {}", x, y);
+    if cpu.registers[x as usize] == cpu.registers[y as usize] {
+        cpu.pc += 2;
+    }
+}
 
-#[allow(unused_variables)]
-#[allow(dead_code)]
-fn op_6xnn(cpu: &mut Cpu, opcode: u16) {}
+fn op_6xnn(cpu: &mut Cpu, opcode: u16) {
+    let (_, kk, _, x, _) = get_opcode_parts(opcode);
+    println!("6xnn {} {}", x, kk);
+    cpu.registers[x as usize] = kk;
+}
 
-#[allow(unused_variables)]
-#[allow(dead_code)]
-fn op_7xnn(cpu: &mut Cpu, opcode: u16) {}
+fn op_7xnn(cpu: &mut Cpu, opcode: u16) {
+    let (_, kk, _, x, _) = get_opcode_parts(opcode);
+    println!("7xnn {} {}", x, kk);
+    cpu.registers[x as usize].wrapping_add(kk);
+}
 
-#[allow(unused_variables)]
-#[allow(dead_code)]
-fn op_8xy0(cpu: &mut Cpu, opcode: u16) {}
+fn op_8xy0(cpu: &mut Cpu, opcode: u16) {
+    let (_, _, _, x, y) = get_opcode_parts(opcode);
+    println!("8xy0 {} {}", x, y);
+    cpu.registers[x as usize] = cpu.registers[y as usize];
 
-#[allow(unused_variables)]
-#[allow(dead_code)]
-fn op_8xy1(cpu: &mut Cpu, opcode: u16) {}
+}
+
+fn op_8xy1(cpu: &mut Cpu, opcode: u16) {
+    
+}
 
 #[allow(unused_variables)]
 #[allow(dead_code)]
